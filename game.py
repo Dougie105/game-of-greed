af5cde83374683727df0df406a4f613f018cab32
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-12-29 09:40:29 -0800

    redid entire assignment

--- game.py
+++ game.py
@@ -1,10 +1,8 @@
-""""Game Module Docstring"""
-
 import random
 from collections import Counter
 
 
-class CheatingScoundrelError(ValueError):
+class CheatingError(ValueError):
     pass
 
 
@@ -17,22 +15,18 @@ class Game:
         self.score = 0
         self.num_rounds = 10
 
-
     def play(self, num_rounds=10):
 
         self.num_rounds = num_rounds
 
-        self._print('Welcome to Game of Greed')
+        self._print("Welcome to Game of Greed")
 
-        response = self._input('Wanna play? ')
+        response = self._input("Wanna play? ")
 
-        if response == 'y':
+        if response == "y":
             self._start()
         else:
-            self._print('OK. Maybe later')
-
-
-
+            self._print("OK. Maybe later")
 
     def _start(self):
 
@@ -43,55 +37,51 @@ class Game:
         for i in range(1, self.num_rounds + 1):
             round_score = self._do_round()
 
-            self._print(f'You banked {round_score} points in round {round_num}')
+            self._print(f"You banked {round_score} points in round {round_num}")
 
             self.score += round_score
 
             round_num += 1
 
-            self._print(f'You have {self.score} points total')
+            self._print(f"You have {self.score} points total")
 
-
-        self._print('Thanks for playing!')
-
+        self._print("Thanks for playing!")
 
     def _do_round(self):
         num_dice = 6
 
         score = 0
 
-        while(True):
-            self._print(f'Rolling {num_dice} dice')
+        while True:
+            self._print(f"Rolling {num_dice} dice")
 
             roll = self._do_roll(num_dice)
 
             zilch_check_score = self.calculate_score(roll)
 
-            self._print(f'You rolled {roll}')
+            self._print(f"You rolled {roll}")
 
             if zilch_check_score == 0:
-                self._print('Oh noes! Zilch')
+                self._print("Oh noes! Zilch")
                 return 0
 
             keepers = self.validate_roll(roll)
 
-            # TODO: handle non scoring but mysteriously used dice
             num_dice -= len(keepers)
 
             score += self.calculate_score(keepers)
 
-            self._print(f'You can bank {score} points or try for more')
+            self._print(f"You can bank {score} points or try for more")
 
             if num_dice == 0:
                 num_dice = 6
 
-            self._print(f'You have {num_dice} dice remaining')
+            self._print(f"You have {num_dice} dice remaining")
 
-            roll_again_response = self._input('Roll again? ')
+            roll_again_response = self._input("Roll again? ")
 
-            if not roll_again_response == 'y':
+            if not roll_again_response == "y":
                 break
-
 
         return score
 
@@ -99,26 +89,21 @@ class Game:
 
         while True:
 
-            keep_response = self._input('Enter dice to keep: ')
+            keep_response = self._input("Enter dice to keep: ")
 
             keepers = tuple(int(char) for char in keep_response)
 
             if self.validate(roll, keepers):
                 return keepers
             else:
-                self._print('No way pal')
+                self._print("No way pal")
                 self._print(roll)
-
-
-
-
 
     def validate(self, roll, keepers):
 
         roll_counter = Counter(roll)
         keepers_counter = Counter(keepers)
         return len(keepers_counter - roll_counter) == 0
-
 
     def calculate_score(self, dice):
 
@@ -160,13 +145,11 @@ class Game:
                 else:
                     score += key * 100
 
-        # must be a straight
         if len(dice_counter) == 6:
             score = 1500
             ones_used = True
             fives_used = True
 
-        # must be 3 pairs
         if len(dice_counter) == 3 and dice_counter.most_common()[0][1] == 2:
             ones_used = True
             fives_used = True
--- game_of_greed.py
+++ game_of_greed.py
@@ -1,163 +1,155 @@
-# import random
-# from collections import Counter
-
-# def roll_dice(self):
-
-#     '''
-#     Simulates rolling 6 dice and appends those values into the below dice object. 
-#     '''
-#     return tuple(random.randint(1,6) for _ in range(number_of_dice)) 
-#     # dice = []
-#     # dice.append(random.randint(1, 6))
-#     # dice.append(random.randint(1, 6))
-#     # dice.append(random.randint(1, 6))
-#     # dice.append(random.randint(1, 6))
-#     # dice.append(random.randint(1, 6))
-#     # dice.append(random.randint(1, 6))
-#     # return dice
-
-# class Game:
-#     def __init__(self, print_func=print, input_func=input):
-#         self._print = print_func
-#         self._input = input_func
+import random
+from collections import Counter
 
 
-#     def score_game(self, dice):
-#         '''
-#         Every possibiblity of scoring has its own instance. Starting with the most rare case to the most common case to avoid any miscalculations.
-#         '''
-#         total = 0
-#         cnt = Counter(dice)
+class Game:
+    def __init__(self, print_func=print, input_func=input):
+        self._print = print_func
+        self._input = input_func
 
-#         '''
-#         What the counter is doing is adding the dice to a dictionary where it is then adding up the values of each value. So say you roll 4 2s, the dictionary under the key of 2 will be 4 as a value.
-#         '''
+    def roll_dice(self):
 
-#         one = 100
-#         five = 50
+        """
+        Simulates rolling 6 dice and appends those values into the below dice object.
+        """
 
-#         for key, value in cnt.items():
+        dice = []
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
 
-#             # Straight Score
-#             if (
-#                 value == 1
-#                 and value == 2
-#                 and value == 3
-#                 and value == 4
-#                 and value == 5
-#                 and value == 6
-#             ):
-#                 total += 1500
-#                 break
+        return dice
 
-#             # One Score
-#             if key == 1:
-#                 if value == 6:
-#                     total += one * 40
-#                 elif value == 5:
-#                     total += one * 30
-#                 elif value == 4:
-#                     total += one * 20
-#                 elif value == 3:
-#                     total += one * 10
-#                 else:
-#                     total += value * one
+    # Calculate the score
 
-#             # Two Score
-#             if key == 2:
-#                 if value == 6:
-#                     total += 800
-#                 elif value == 5:
-#                     total += 600
-#                 elif value == 4:
-#                     total += 400
-#                 elif value == 3:
-#                     total += 200
-#                 else:
-#                     total += 0
+    def score_game(self, dice):
+        """
+        Every possibiblity of scoring has its own instance. Starting with the most rare case to the most common case to avoid any miscalculations.
+        """
+        total = 0
+        cnt = Counter(dice)
 
-#             # Three Score
-#             if key == 3:
-#                 if value == 6:
-#                     total += 1200
-#                 elif value == 5:
-#                     total += 900
-#                 elif value == 4:
-#                     total += 600
-#                 elif value == 3:
-#                     total += 300
-#                 else:
-#                     total += 0
+        """
+        What the counter is doing is adding the dice to a dictionary where it is then adding up the values of each value. So say you roll 4 2s, the dictionary under the key of 2 will be 4 as a value.
+        """
 
-#             # Four Score
-#             if key == 4:
-#                 if value == 6:
-#                     total += 1600
-#                 elif value == 5:
-#                     total += 1200
-#                 elif value == 4:
-#                     total += 800
-#                 elif value == 3:
-#                     total += 400
-#                 else:
-#                     total += 0
+        one = 100
+        five = 50
 
-#             # Five Score
-#             if key == 5:
-#                 if value == 6:
-#                     total += five * 40
-#                 elif value == 5:
-#                     total += five * 30
-#                 elif value == 4:
-#                     total += five * 20
-#                 elif value == 3:
-#                     total += five * 10
-#                 else:
-#                     total += value * five
+        for key, value in cnt.items():
 
-#             # Six Score
-#             if key == 6:
-#                 if value == 6:
-#                     total += 2400
-#                 elif value == 5:
-#                     total += 1800
-#                 elif value == 4:
-#                     total += 1200
-#                 elif value == 3:
-#                     total += 600
-#                 else:
-#                     total += 0
-            
-#         return total    
+            # Straight Score
+            if (
+                value == 1
+                and value == 2
+                and value == 3
+                and value == 4
+                and value == 5
+                and value == 6
+            ):
+                total += 1500
+                break
 
-#         print(total)
+            # One Score
+            if key == 1:
+                if value == 6:
+                    total += one * 40
+                elif value == 5:
+                    total += one * 30
+                elif value == 4:
+                    total += one * 20
+                elif value == 3:
+                    total += one * 10
+                else:
+                    total += value * one
 
-#     def play(self, user_response=None):
-#         pockets = []
+            # Two Score
+            if key == 2:
+                if value == 6:
+                    total += 800
+                elif value == 5:
+                    total += 600
+                elif value == 4:
+                    total += 400
+                elif value == 3:
+                    total += 200
+                else:
+                    total += 0
 
-#         '''
-#         Prints a prompt in the beginning of terminal and prints greeting accordingly.
-#         '''
-        
-#         self._print("Welcome to Game of Greed!")
-#         response = self._input("Wanna play? ")
+            # Three Score
+            if key == 3:
+                if value == 6:
+                    total += 1200
+                elif value == 5:
+                    total += 900
+                elif value == 4:
+                    total += 600
+                elif value == 3:
+                    total += 300
+                else:
+                    total += 0
 
-#         keep = self._input('Would you like to keep any die? ')
+            # Four Score
+            if key == 4:
+                if value == 6:
+                    total += 1600
+                elif value == 5:
+                    total += 1200
+                elif value == 4:
+                    total += 800
+                elif value == 3:
+                    total += 400
+                else:
+                    total += 0
+
+            # Five Score
+            if key == 5:
+                if value == 6:
+                    total += five * 40
+                elif value == 5:
+                    total += five * 30
+                elif value == 4:
+                    total += five * 20
+                elif value == 3:
+                    total += five * 10
+                else:
+                    total += value * five
+
+            # Six Score
+            if key == 6:
+                if value == 6:
+                    total += 2400
+                elif value == 5:
+                    total += 1800
+                elif value == 4:
+                    total += 1200
+                elif value == 3:
+                    total += 600
+                else:
+                    total += 0
+
+        return total
+
+        print(total)
+
+    def play(self, user_response=None):
+
+        """
+        Prints  Prints a beginning prompt to the user and greeting.
+        """
+
+        self._print("Welcome to Game of Greed!")
+        response = self._input("Wanna play? ")
+
+        if response == "y" or user_response == "y":
+            self._print("Great! Check back tomorrow!")
+        else:
+            self._print("OK. Maybe another time")
 
 
-#         if response == "y" or user_response == "y":
-#             self._print("Great! Check back tomorrow!")
-#             if keep == 'y' or user_response == 'y':
-#                 pocket = self._input('Which would you like to keep? ')
-#                 pockets.append(pockets)
-#             else:
-#                 roll_dice()
-#         else:
-#             self._print("OK. Maybe another time")
-
-#         print(f'pocket: {pockets}')
-
-
-# if __name__ == "__main__":
-#     game = Game()
-#     game.play()
+if __name__ == "__main__":
+    game = Game()
+    game.play()
